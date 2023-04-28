a0705ee5904bb367c3e4c213a9caae7008af092d
Srat00 <rockyeo33@gmail.com>
2023-04-29 04:07:41 +0900

    Update pyGameTest.py

--- pyGameTest.py
+++ pyGameTest.py
@@ -18,74 +18,136 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
-# 플레이어 설정
-player_image = pygame.image.load("image/player.png").convert()
-player_image.set_colorkey(WHITE)
-player_rect = player_image.get_rect()
-player_rect.centerx = SCREEN_WIDTH / 2
-player_rect.bottom = SCREEN_HEIGHT / 2
-player_speed = 5
+# 객체 정의
+class Player:
+    def __init__(self):
+        self.x = SCREEN_WIDTH // 2
+        self.y = SCREEN_HEIGHT // 2
+        self.angle = 0
+        self.speed = 5
+        
+        self.image = pygame.image.load("image/player.png").convert_alpha()
+        self.rotated_image = pygame.transform.rotate(self.image, self.angle).convert_alpha()
+        
+        self.mask = pygame.mask.from_surface(self.rotated_image)
+        self.rect = self.rotated_image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
+    def rotate(self, angle):
+        self.rotated_image = pygame.transform.rotate(self.image, self.angle)
 
-# 좀비 설정
-zombie_image = pygame.image.load("image/zombie.png").convert()
-zombie_image.set_colorkey(WHITE)
+    def draw(self):
+        screen.blit(self.rotated_image, (self.x + self.image.get_width() // 2 - self.rotated_image.get_width() // 2,
+                                         self.y + self.image.get_height() // 2 - self.rotated_image.get_height() // 2))
+    
+    def move_up(self):
+        self.y -= self.speed
+    
+    def move_down(self):
+        self.y += self.speed
+        
+    def move_left(self):
+        self.x -= self.speed
+        
+    def move_right(self):
+        self.x += self.speed
+        
+class Zombie:
+    def __init__(self):
+        self.angle = 0
+        self.speed = 2
+        self.x = random.randrange(SCREEN_WIDTH)
+        self.y = random.randrange(SCREEN_HEIGHT)
+        
+        self.image = pygame.image.load("image/zombie.png").convert_alpha()
+        self.rotated_image = pygame.transform.rotate(self.image, self.angle).convert_alpha()
+        
+        self.mask = pygame.mask.from_surface(self.rotated_image)
+        self.rect = self.rotated_image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
+    def draw(self):
+        screen.blit(self.rotated_image, (self.x + self.image.get_width() // 2 - self.rotated_image.get_width() // 2,
+                                         self.y + self.image.get_height() // 2 - self.rotated_image.get_height() // 2))
+        
+    def move_up(self):
+        self.y -= self.speed
+    
+    def move_down(self):
+        self.y += self.speed
+        
+    def move_left(self):
+        self.x -= self.speed
+        
+    def move_right(self):
+        self.x += self.speed
+
+
+# 환경 변수
+zombie_amount = 10
 zombie_list = []
-zombie_rect_list = []
-for i in range(10):
-    print("zombie", i)
-    zombie_rect = zombie_image.get_rect()
-    zombie_rect.x = random.randrange(player_rect.x - 1000, player_rect.x + 1000)
-    zombie_rect.y = random.randrange(player_rect.y - 1000, player_rect.y + 1000)
-    zombie_speed = 2
-    zombie_list.append({'rect': zombie_rect, 'speed': zombie_speed})
-    zombie_rect_list.append(zombie_rect)
+
 
 # 게임 루프
 done = False
 clock = pygame.time.Clock()
 
+# 초기화
+
+# 플레이어 생성
+player = Player()
+# 적 생성
+for i in range(zombie_amount):
+    zombie_list.append(Zombie())
+
+#게임 루프
 while not done:
     # 이벤트 처리
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-
+            
     # 플레이어 이동 처리
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        player_rect.x -= player_speed
+        player.move_left()
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        player_rect.x += player_speed
+        player.move_right()
     if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-        player_rect.y += player_speed    
+        player.move_down()
     if keys[pygame.K_w] or keys[pygame.K_UP]:
-        player_rect.y -= player_speed
-
-    for zombie in zombie_list:
-        # 좀비 이동 처리
-        dx = player_rect.centerx - zombie['rect'].centerx
-        dy = player_rect.centery - zombie['rect'].centery
-        dist = math.hypot(dx, dy)
-        dx = dx / dist
-        dy = dy / dist
-        zombie_rect = zombie['rect'].copy()
-        zombie_rect.x += dx * zombie['speed']
-        zombie_rect.y += dy * zombie['speed']
+        player.move_up()
         
+    # 종합 좀비 이벤트 처리
+    for i in range(zombie_amount):
+        # 좀비 이동 처리
+        if player.x > zombie_list[i].x:
+            zombie_list[i].move_right()
+        if player.x < zombie_list[i].x:
+            zombie_list[i].move_left()
+        if player.y > zombie_list[i].y:   
+            zombie_list[i].move_down()
+        if player.y < zombie_list[i].y:
+            zombie_list[i].move_up()
+            
+        # 좀비 충돌 처리
+        if pygame.sprite.collide_mask(player, zombie_list[i]):
+            print("HIT")
+            zombie_list[i].x = random.randrange(SCREEN_WIDTH)
+            zombie_list[i].y = random.randrange(SCREEN_HEIGHT)
 
-    # 충돌 검사
-    for zombie in zombie_list:
-        if player_rect.colliderect(zombie['rect']):
-            done = True
-
-    # 그리기
-    screen.fill(WHITE)
-    screen.blit(player_image, player_rect)
-    for zombie in zombie_list:
-        screen.blit(zombie_image, zombie['rect'])
 
     # 업데이트
-    pygame.display.flip()
+    screen.fill(WHITE)
+    
+    player.draw()
+    
+    for i in range(zombie_amount):
+        zombie_list[i].draw()
+   
+    pygame.display.update()
 
     # 초당 프레임 수 설정
     clock.tick(60)
