881521d163329ddfa2efa42492a04e60abc58e4b
Srat00 <rockyeo33@gmail.com>
2023-06-13 14:15:49 +0900

    Final Update!
    Final!

--- WEB/web_study/settings.py
+++ WEB/web_study/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2ui=vfqn*!qw#bv5(9l3h_%sk2ul8vk@3zkybu0g+cob%kcj)m'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['INSERT_YOUR_IP_ADDRESS']
 
 
 # Application definition
